274645c7aca6b792cecaa2a7637251ebfdbab7fa
Sadik <sadikavllaj55@gmail.com>
2025-04-22 15:21:15 +0200

    Update checkout

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -1,64 +1,49 @@
 @extends('layouts.front')
 
-
 @section('content')
 <!-- main wrapper -->
 <section class="mb-lg-14 mb-8 mt-8">
     <div class="container">
         <div class="row">
             <div class="col-lg-7 col-md-12">
-                <div class="d-flex justify-content-between mb-5">
-                    <!-- heading -->
-                    <div>
-                        <h5 class="h6 mb-1" id="addAddressModalLabel">New Shipping Address</h5>
-                        <p class="small mb-0">Add new shipping address for your order delivery.</p>
-                    </div>
-                </div>
-                <!-- row -->
                 <form method="post" action="{{ route('order.store') }}">
+                    @csrf
                     <div class="row g-3">
                         <div class="mt-5">
                             <div>
+                                <div class="d-flex mb-4">
+                                    <div>
+                                        <h5 class="mb-1 h6"> Credit / Debit Card</h5>
+                                        <p class="mb-0 small">Safe money transfer using your bank accou k account. We support
+                                            Mastercard tercard, Visa, Discover and Stripe.</p>
+                                    </div>
+                                </div>
                                 <div class="card card-bordered shadow-none mb-2">
                                     <!-- card body -->
                                     <div class="card-body p-6">
-                                        <div class="d-flex mb-4">
-                                            <div>
-                                                <h5 class="mb-1 h6"> Credit / Debit Card</h5>
-                                                <p class="mb-0 small">Safe money transfer using your bank accou k account. We support
-                                                    Mastercard tercard, Visa, Discover and Stripe.</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
+                                        <div class="row g-2">
                                             <div class="col-12">
-                                                <!-- input -->
                                                 <div class="mb-3">
-                                                    <label class="form-label">Card Number</label>
-                                                    <input type="text" class="form-control" placeholder="1234 4567 6789 4321" name="card_number" required>
+                                                    <label for="card_nr" class="form-label">Card Number</label>
+                                                    <input type="text" class="form-control" name="card_nr" id="card_nr" placeholder="XXXX XXXX XXXX XXXX" required>
                                                 </div>
                                             </div>
                                             <div class="col-md-6 col-12">
-                                                <!-- input -->
                                                 <div class="mb-3 mb-lg-0">
-                                                    <label class="form-label">Name on card </label>
-                                                    <input type="text" class="form-control" placeholder="Enter your first name" name="card_name" required>
+                                                    <label class="form-label" for="card_name">Name on card</label>
+                                                    <input type="text" class="form-control" placeholder="Enter name" name="card_name" id="card_name" required>
                                                 </div>
                                             </div>
                                             <div class="col-md-3 col-12">
-                                                <!-- input -->
-                                                <div class="mb-3  mb-lg-0 position-relative">
-                                                    <label class="form-label">Expiry date </label>
-                                                    <input class="form-control flatpickr " type="text" placeholder="Select Date">
-                                                    <div class="position-absolute bottom-0 end-0 p-3 lh-1">
-                                                        <i class="bi bi-calendar text-muted"></i>
-                                                    </div>
+                                                <div class="mb-3 mb-lg-0 position-relative">
+                                                    <label class="form-label" for="card_expiry">Expiry date</label>
+                                                    <input type="text" class="form-control" name="card_expiry" id="card_expiry" placeholder="MM/YY" required>
                                                 </div>
                                             </div>
                                             <div class="col-md-3 col-12">
-                                                <!-- input -->
-                                                <div class="mb-3  mb-lg-0">
-                                                    <label class="form-label">CVV code </label>
-                                                    <input type="text" class="form-control" placeholder="312" name="cvv" required>
+                                                <div class="mb-3 mb-lg-0">
+                                                    <label for="card_cvc" class="form-label">CVV Code</label>
+                                                    <input type="password" class="form-control" name="card_cvc" id="card_cvc" placeholder="XXX" maxlength="3" inputmode="numeric" required>
                                                 </div>
                                             </div>
                                         </div>
@@ -66,9 +51,13 @@
                                 </div>
                             </div>
                         </div>
-                    <!-- col -->
-
-                        @csrf
+                        <div class="d-flex justify-content-between mb-5">
+                            <!-- heading -->
+                            <div>
+                                <h5 class="h6 mb-1" id="addAddressModalLabel">New Shipping Address</h5>
+                                <p class="small mb-0">Add new shipping address for your order delivery.</p>
+                            </div>
+                        </div>
                         <div class="col-12">
                             <input type="text" name="first_name" class="form-control" placeholder="First name" required>
                         </div>
@@ -159,14 +148,45 @@
                         </ul>
 
                     </div>
-
-
                 </div>
             </div>
         </div>
     </div>
 </section>
-<script src="{{ asset('assets/js/flatpickr.min.js') }}"></script>
-
 @endsection
 
+@section('scripts')
+    <script src="https://unpkg.com/imask"></script>
+    <script>
+        function setUpPaymentInputs() {
+            const card_number_input = document.getElementById('card_nr');
+            const expiry_date_input = document.getElementById('card_expiry');
+            const cvc_input = document.getElementById('card_cvc');
+
+            const card_mask = new IMask(card_number_input, {
+                mask: '0000 0000 0000 0000'
+            });
+            const expiry_mask = new IMask(expiry_date_input, {
+                mask: 'MM / YY',
+                blocks: {
+                    MM: {
+                        mask: IMask.MaskedRange,
+                        from: 1,
+                        to: 12,
+                        autofix: 'pad'
+                    },
+                    YY: {
+                        mask: '00'
+                    }
+                }
+            });
+            const cvc_mask = new IMask(cvc_input, {
+                mask: '000'
+            });
+        }
+
+        window.addEventListener('load', () => {
+            setUpPaymentInputs();
+        })
+    </script>
+@endsection
